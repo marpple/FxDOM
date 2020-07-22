511a480674d74475b31ead028713c20b64431a3a
daengdaenglee <gunho1020@gmail.com>
2020-07-22 21:14:07 +0900

    브라우저에서 mocha + chai 조합 테스트 환경 구축

---
+++ test/test.js
@@ -0,0 +1,11 @@
+import { expect } from "chai";
+
+describe(`FxDOM`, function () {
+  it(`1 + 1 = 2`, function () {
+    expect(1 + 1).equal(2);
+  });
+
+  it(`true === false`, function () {
+    expect(true).false;
+  });
+});
