9e0e88ff0538cc968c33c8ef63047d64d82d5420
daengdaenglee <gunho1020@gmail.com>
2020-07-28 21:08:55 +0900

    addClass 함수 테스트 작성

---
+++ test/suits/addClass.spec.js
@@ -0,0 +1,48 @@
+import { expect } from "chai";
+import { html } from "fxjs2";
+import addClass from "../../src/addClass.js";
+import el from "../../src/el.js";
+
+export default ({ describe, it }) => [
+  describe(`addClass`, function () {
+    it(`반환값은 입력한 엘리먼트이다.`, function () {
+      // given
+      const $el = el(html`<div></div>`);
+      const class_names = "a";
+
+      // when
+      const $output = addClass(class_names, $el);
+
+      // then
+      expect($output).equal($el);
+    });
+
+    it(`입력한 클래스명을 입력한 엘리먼트에 부여한다.`, function () {
+      // given
+      const $el = el(html`<div></div>`);
+      const class_name = "a";
+
+      // when
+      addClass(class_name, $el);
+
+      // then
+      expect($el.classList.contains(class_name)).true;
+    });
+
+    it(`여러 클래스명을 입력할 경우 공백으로 분리하여 각각 입력한 엘리먼트에 부여한다.`, function () {
+      // given
+      const $el = el(html`<div></div>`);
+      const class_name1 = "a";
+      const class_name2 = "b";
+      const class_names = `${class_name1} ${class_name2}`;
+
+      // when
+      addClass(class_names, $el);
+
+      // then
+      const { classList: class_list } = $el;
+      expect(class_list.contains(class_name1)).true;
+      expect(class_list.contains(class_name2)).true;
+    });
+  }),
+];
--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 import { each } from "fxjs2";
+import __addClass_spec__ from "./suits/addClass.spec.js";
 import __el_spec__ from "./suits/el.spec.js";
 
-each((spec) => spec({ describe, it }), [__el_spec__]);
+each((spec) => spec({ describe, it }), [__addClass_spec__, __el_spec__]);
