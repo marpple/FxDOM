9cc7eff589af9f70e0abb766f60bed5594f02ccb
daengdaenglee <gunho1020@gmail.com>
2020-08-14 16:48:29 +0900

    _baseOnOff 함수 코드 정리

--- src/.internal/_baseOnOff.js
+++ src/.internal/_baseOnOff.js
@@ -1,13 +1,9 @@
-import { tap, isString, go, each } from "fxjs2";
+import { isString, each } from "fxjs2";
 import findAll from "../findAll.js";
 
-export default (method) => (event, sel, f, ...opts) =>
-  tap((el) =>
-    isString(sel)
-      ? go(
-          el,
-          findAll(sel),
-          each((el) => el[method](event, f, ...opts))
-        )
-      : el[method](event, sel, ...[f, ...opts])
-  );
+export default (method) => (event, f, ...opts) => (el) => (
+  isString(f)
+    ? each((el) => el[method](event, ...opts), findAll(f, el))
+    : el[method](event, f, ...opts),
+  el
+);
